aba5d6f312cabc634e4b8479b364f03abb5c3853
Julia3002 <juliamvuzi3@gmail.com>
2025-06-17 17:07:15 -0400

    Update app.py

--- dash_app/app.py
+++ dash_app/app.py
@@ -1,9 +1,12 @@
+
 import dash
 from dash import html, dcc
 import requests
 
-# Initialiser l'application Dash
-app = dash.Dash(__name__, requests_pathname_prefix='/dashboard/')
+
+
+# initializer 'application Dash
+app = dash.Dash(__name__, requests_pathname_prefix ='/dashboard/')
 
 def get_weather_info():
     try:
@@ -17,19 +20,17 @@ def get_weather_info():
                 "description": "Erreur"
             }
         }
-
-# Appeler la météo une seule fois au chargement
 weather = get_weather_info()
 
-# Layout avec météo + graphiques
+# Definir Dash layout avec 4 graph
 app.layout = html.Div(children=[
     html.Div([
-        html.A('Accueil', href='/'),
+        html.A('Acceuil', href='/'),
         "|",
         html.A("Logout", href="/logout")
-    ], style={'marginTop': 25}),
+    ], style={'marginTop':25}),
     
-    html.H1("Example de Dashboard"),
+    html.H1(children = "Example de Dashboard" ),
 
     html.H3(f"Météo à {weather['weather']['city']}"),
     html.P(f"Température : {weather['weather']['temperature']} °C"),
@@ -39,43 +40,51 @@ app.layout = html.Div(children=[
     dcc.Graph(
         id="exm1",
         figure={
-            "data": [
-                {"x": [5, 7, 12], "y": [10, 16, 11], "type": "bar", "name": "exmple1"},
-                {"x": [8, 18, 22], "y": [5, 8, 3], "type": "bar", "name": "exmple2"}
+            "data":[
+                {"x":[5,7,12], "y":[10,16,11] , "type":"bar" , "name":"exmple1"},
+                {"x":[8,18,22], "y":[5,8,3] , "type":"bar" , "name":"exmple2"}
             ]
         }
     ),
-
+    
     html.H2("*** Line Graph *** "),
     dcc.Graph(
         id="exm2",
         figure={
-            "data": [
-                {"x": [1, 3, 5], "y": [10, 12, 14], "type": "line", "name": "exmple3"},
-                {"x": [2, 4, 6], "y": [13, 15, 17], "type": "line", "name": "exmple4"}
+            "data":[
+                {"x":[1,3,5], "y":[10,12,14] , "type":"line" , "name":"exmple3"},
+                {"x":[2,4,6], "y":[13,15,17] , "type":"line" , "name":"exmple4"}
             ]
         }
     ),
-
-    html.H2("*** Scatter Plot Graph *** "),
+    
+    html.H2("*** scatter Plot Graph *** "),
     dcc.Graph(
         id="exm3",
         figure={
-            "data": [
-                {"x": [1, 3, 5, 7], "y": [10, 12, 14, 16], "type": "scatter", "mode": "markers", "name": "scatter exmpl1"},
-                {"x": [2, 4, 6, 8], "y": [13, 15, 17, 19], "type": "scatter", "mode": "markers", "name": "scatter exmpl2"}
+            "data":[
+                {"x":[1,3,5,7], "y":[10,12,14,16] , "type":"scatter" , "mode":"markers","name":"scatter exmpl1"},
+                {"x":[2,4,6,8], "y":[13,15,17,19] , "type":"scatter" , "mode":"markers","name":"scatter exmpl2"}
             ]
         }
     ),
-
+    
     html.H2("*** Pie Chart Graph *** "),
     dcc.Graph(
         id="exm4",
         figure={
-            "data": [
-                {"labels": ["A", "B", "C"], "y": [10, 12, 14], "type": "pie", "name": "pie chart expl1"},
+            "data":[
+                {"labels":["A","B","C"], "y":[10,12,14] , "type":"pie" , "name":"pie chart expl1"},
             ],
-            "layout": {"title": "pie chart example"}
+            "layout":{"title":"pie chart example"}
         }
-    )
+    ),
+
+    
+    
+    
+
 ])
+
+
+server = app.server
